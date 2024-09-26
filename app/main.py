d54c228c381a98d480f71c370aca5e2676dfa2ba
Ярослав Шевченко <yaroslav_0205@MacBook-Pro-Aroslav.local>
2024-09-26 18:24:28 +0300

    car_wash_station_4

--- app/main.py
+++ app/main.py
@@ -22,11 +22,11 @@ class CarWashStation:
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(
+        return round((
             car.comfort_class * (self.clean_power - car.clean_mark) *
             self.average_rating /
-            self.distance_from_city_center, 1
-        )
+            self.distance_from_city_center
+        ), 1)
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
@@ -34,8 +34,8 @@ class CarWashStation:
 
     def rate_service(self, rating: int) -> None:
         self.count_of_ratings += 1
-        self.average_rating = round(
+        self.average_rating = round((
             (self.average_rating * (self.count_of_ratings - 1) +
              rating) /
-            self.count_of_ratings, 1
-        )
+            self.count_of_ratings
+        ), 1)
