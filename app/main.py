c07a7ff6f930676e4583423298596fdeebbf061b
Ярослав Шевченко <yaroslav_0205@MacBook-Pro-Aroslav.local>
2024-09-26 21:56:09 +0300

    made_the_line_breaks

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 class Car:
-    def __init__(self, comfort_class: int,
+    def __init__(self,
+                 comfort_class: int,
                  clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
@@ -7,7 +8,8 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float,
+    def __init__(self,
+                 distance_from_city_center: float,
                  clean_power: int,
                  average_rating: float,
                  count_of_ratings: int) -> None:
